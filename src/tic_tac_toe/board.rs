0ea7b92499924d678bca6e987b88b0955fb006e3
Andrew Phoenix <mrtopnacho@gmail.com>
2024-02-23 18:35:51 -0500

    Fixed logical bug in get_winner

--- src/tic_tac_toe/board.rs
+++ src/tic_tac_toe/board.rs
@@ -1,5 +1,5 @@
-use std::ops::{Index, IndexMut};
 use serde::{Deserialize, Serialize};
+use std::ops::{Index, IndexMut};
 
 use super::cell::{Cell, CellIndex};
 
@@ -16,25 +16,40 @@ impl Board {
     }
     pub fn get_winner(&self) -> Option<Winner> {
         // Check columns
-        self.check_columns();
-        // Check rows
-        self.check_rows();
-        // Check diagonals
-        self.check_diagonals();
-        return None;
+        return if let Some(winner) = self.check_columns() {
+            Some(winner)
+        } else if let Some(winner) = self.check_rows() {
+            Some(winner)
+        } else if let Some(winner) = self.check_diagonals() {
+            Some(winner)
+        } else {
+            None
+        };
     }
     fn check_columns(&self) -> Option<Winner> {
-        match [self[CellIndex::TOP_LEFT], self[CellIndex::MIDDLE_LEFT], self[CellIndex::BOTTOM_LEFT]] {
+        match [
+            self[CellIndex::TOP_LEFT],
+            self[CellIndex::MIDDLE_LEFT],
+            self[CellIndex::BOTTOM_LEFT],
+        ] {
             [Cell::X, Cell::X, Cell::X] => return Some(Winner::X),
             [Cell::O, Cell::O, Cell::O] => return Some(Winner::O),
             _ => (),
         };
-        match [self[CellIndex::TOP_MIDDLE], self[CellIndex::CENTER], self[CellIndex::BOTTOM_MIDDLE]] {
+        match [
+            self[CellIndex::TOP_MIDDLE],
+            self[CellIndex::CENTER],
+            self[CellIndex::BOTTOM_MIDDLE],
+        ] {
             [Cell::X, Cell::X, Cell::X] => return Some(Winner::X),
             [Cell::O, Cell::O, Cell::O] => return Some(Winner::O),
             _ => (),
         };
-        match [self[CellIndex::TOP_RIGHT], self[CellIndex::MIDDLE_RIGHT], self[CellIndex::BOTTOM_RIGHT]] {
+        match [
+            self[CellIndex::TOP_RIGHT],
+            self[CellIndex::MIDDLE_RIGHT],
+            self[CellIndex::BOTTOM_RIGHT],
+        ] {
             [Cell::X, Cell::X, Cell::X] => return Some(Winner::X),
             [Cell::O, Cell::O, Cell::O] => return Some(Winner::O),
             _ => (),
@@ -42,17 +57,29 @@ impl Board {
         return None;
     }
     fn check_rows(&self) -> Option<Winner> {
-        match [self[CellIndex::TOP_LEFT], self[CellIndex::TOP_MIDDLE], self[CellIndex::TOP_RIGHT]] {
+        match [
+            self[CellIndex::TOP_LEFT],
+            self[CellIndex::TOP_MIDDLE],
+            self[CellIndex::TOP_RIGHT],
+        ] {
             [Cell::X, Cell::X, Cell::X] => return Some(Winner::X),
             [Cell::O, Cell::O, Cell::O] => return Some(Winner::O),
             _ => (),
         }
-        match [self[CellIndex::MIDDLE_LEFT], self[CellIndex::CENTER], self[CellIndex::MIDDLE_RIGHT]] {
+        match [
+            self[CellIndex::MIDDLE_LEFT],
+            self[CellIndex::CENTER],
+            self[CellIndex::MIDDLE_RIGHT],
+        ] {
             [Cell::X, Cell::X, Cell::X] => return Some(Winner::X),
             [Cell::O, Cell::O, Cell::O] => return Some(Winner::O),
             _ => (),
         }
-        match [self[CellIndex::BOTTOM_LEFT], self[CellIndex::BOTTOM_MIDDLE], self[CellIndex::BOTTOM_RIGHT]] {
+        match [
+            self[CellIndex::BOTTOM_LEFT],
+            self[CellIndex::BOTTOM_MIDDLE],
+            self[CellIndex::BOTTOM_RIGHT],
+        ] {
             [Cell::X, Cell::X, Cell::X] => return Some(Winner::X),
             [Cell::O, Cell::O, Cell::O] => return Some(Winner::O),
             _ => (),
@@ -60,12 +87,20 @@ impl Board {
         return None;
     }
     fn check_diagonals(&self) -> Option<Winner> {
-        match [self[CellIndex::TOP_LEFT], self[CellIndex::CENTER], self[CellIndex::BOTTOM_RIGHT]] {
+        match [
+            self[CellIndex::TOP_LEFT],
+            self[CellIndex::CENTER],
+            self[CellIndex::BOTTOM_RIGHT],
+        ] {
             [Cell::X, Cell::X, Cell::X] => return Some(Winner::X),
             [Cell::O, Cell::O, Cell::O] => return Some(Winner::O),
             _ => (),
         }
-        match [self[CellIndex::TOP_RIGHT], self[CellIndex::CENTER], self[CellIndex::BOTTOM_LEFT]] {
+        match [
+            self[CellIndex::TOP_RIGHT],
+            self[CellIndex::CENTER],
+            self[CellIndex::BOTTOM_LEFT],
+        ] {
             [Cell::X, Cell::X, Cell::X] => return Some(Winner::X),
             [Cell::O, Cell::O, Cell::O] => return Some(Winner::O),
             _ => (),
@@ -80,10 +115,11 @@ impl Board {
     pub fn iter_enumerated(&self) -> impl Iterator<Item = (CellIndex, Cell)> {
         return self.iter().enumerate().map(|(i, cell)| {
             (
-                CellIndex::try_from(i).expect("`Board::iter` has 9 elements so `i` will never be larger than eight"),
+                CellIndex::try_from(i)
+                    .expect("`Board::iter` has 9 elements so `i` will never be larger than eight"),
                 cell,
             )
-        })
+        });
     }
     pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut Cell> {
         return self.cells.iter_mut().flatten();
@@ -91,10 +127,12 @@ impl Board {
     pub fn iter_mut_enumerated(&mut self) -> impl Iterator<Item = (CellIndex, &mut Cell)> {
         return self.iter_mut().enumerate().map(|(i, cell)| {
             (
-                CellIndex::try_from(i).expect("`Board::iter_mut` has 9 elements so `i` will never be larger than eight"),
+                CellIndex::try_from(i).expect(
+                    "`Board::iter_mut` has 9 elements so `i` will never be larger than eight",
+                ),
                 cell,
             )
-        })
+        });
     }
 }
 impl IndexMut<CellIndex> for Board {
