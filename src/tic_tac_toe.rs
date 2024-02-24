354b992a9ad83f71f3705fe1b0c2be4856aa1ae9
Andrew Phoenix <mrtopnacho@gmail.com>
2024-02-24 13:30:32 -0500

    Implemented Default manually for GameState

--- src/tic_tac_toe.rs
+++ src/tic_tac_toe.rs
@@ -8,13 +8,21 @@ pub use self::{
 
 pub use serde::{Serialize, Deserialize};
 
-#[derive(Default, Debug, Deserialize, Serialize, Clone, Copy)]
+#[derive(Debug, Deserialize, Serialize, Clone, Copy)]
 pub struct GameState {
     pub board: Board,
     pub is_x_turn: bool,
     pub winner: Option<Winner>,
 }
-
+impl Default for GameState {
+    fn default() -> Self {
+        return Self {
+            board: Board::default(),
+            is_x_turn: true,
+            winner: None,
+        }
+    }
+}
 #[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 pub enum Winner {
     X,
